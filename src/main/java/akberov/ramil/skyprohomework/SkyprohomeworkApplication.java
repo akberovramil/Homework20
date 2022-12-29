2fdb85e5325c46fb84b4a2828536686315a2c2b3
akberovramil <ramil.akberov96@gmail.com>
2022-12-29 18:52:03 +0300

    First commit

---
+++ src/main/java/akberov/ramil/skyprohomework/SkyprohomeworkApplication.java
@@ -0,0 +1,13 @@
+package akberov.ramil.skyprohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyprohomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyprohomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akberov/ramil/skyprohomework/SkyprohomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package akberov.ramil.skyprohomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyprohomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
